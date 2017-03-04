1bae91520ee708696f74a6c5a4c3959d12b40af8
Castlela <240091225@qq.com>
2017-03-04 21:01:58 +0800

    deleted 无用的shoot

--- src/测试Shoot/Enemy.java
+++
@@ -1,5 +0,0 @@
-package 测试Shoot;
-
-public interface Enemy {
-	public int getScore();
-}
--- src/测试day14/Enemy.java
+++
@@ -1,9 +0,0 @@
-/**
- * Enemy敌人接口，airplane敌机扩展接口，为了以后增加bossplane时，准备。
-*/
-package 测试day14;
-public interface Enemy {
-	/** 打倒敌机的得分 */
-	public int getScore();
-	
-}
